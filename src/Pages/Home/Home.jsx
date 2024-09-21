37a106128e72d75b1455117df8769868aa177ea0
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-09-21 23:21:17 +0600

    created faq, pricing, and footer components with responsive styling

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import HandyFreeTools from "@/Pages/Tools/HandyFreeTools";
 
 import Hero from "../../Components/landing/Hero";
 import TemplateSelector from "../../Components/landing/TemplateSelector";
+import FrequentlyAskedQuestion from "../../Components/FrequentlyAskedQuestions/FAQ";
+import Pricing from "../../Components/Pricing/Pricing";
+import FooterSection from "../../Components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -13,6 +16,12 @@ const Home = () => {
           <TemplateSelector />
 
           <HandyFreeTools />
+
+          <FrequentlyAskedQuestion />
+
+          <Pricing />
+
+          <FooterSection />
 
           <Footer></Footer>
         </main>
